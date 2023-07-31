19644ceca9879fc5f73d5f00150ecf6cb7632560
Drew Nutter <dnut@users.noreply.github.com>
2023-07-31 16:33:34 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# HTTP Request Response Client
+
+Client implementation of a common request-response pattern. Simply pass any type into one of the `send` functions or methods, and it will return a result of your desired type. `send` handles serialization, deserialization, and http messaging.
+
+To keep this crate simple, it is is oriented towards a specific but very common pattern. If your use case meets the following conditions, this crate will work for you:
+1. request-response communication
+2. async rust functions
+3. communicate over http (uses reqwest under the hood)
+4. http body is serialized as json
+5. status codes outside the 200 range are considered errors
+6. request and response types can be serialized and deserialized using serde into concrete rust types
+7. the path and HTTP method can be determined from the concrete rust type used for the request
+
+## Usage
+
+If the request and response types already implement serde::Serialize and serde::Deserialize respectively, the most manual and basic way to use this library is by using `send_custom`:
+
+```rust
+let my_response: MyResponse = send_custom(
+    "http://example.com/path/to/my/request/",
+    HttpMethod::Get,
+    MyRequest::new()
+)
+.await?;
+```
+
+The downside to `send_custom` is that you must specify metadata about the request every time you send a request, even though these things will likely be the same for every request of this type. To avoid this, you may describe the request metadata in the type system by implementing the Request trait.
+
+```rust
+pub trait Request {
+    type Response;
+    fn method(&self) -> HttpMethod;
+    fn path(&self) -> String;
+}
+```
+
+This increases design flexibility and can reduce boilerplate. Normally, you might implement a custom gateway struct for every api you interact with, writing a custom send method for every request. Instead, if the metadata is described through the type system, there is no need for custom client structs.
+
+If you do not control the crate with the request and response structs, you can implement any traits for them using the newtype pattern, or with a reusable generic wrapper struct.
+
+After implementing this trait, you can use the send function, which requires the base url to be included, instead of the full url. All other information about how to send the request and response is implied by the type of the input.
+
+```rust
+let my_response = send("http://example.com", MyRequest::new()).await?;
+// The type of my_response is determined by the trait's associated type.
+// It does not need to be inferrable from the calling context.
+return my_response.some_field
+```
+
+If you don't want to include the base url with every call to `send`, instantiate a Client:
+
+```rust
+let client = Client::new("http://example.com");
+let my_response = client.send(MyRequest::new()).await?;
+```
+
+You can also define request groups. This defines a client type that is explicit about exactly which requests it can handle. The code will not compile if you try to send a request with the wrong client. If you want to restrict the request group, but still want to include the url for every call to `send`, `MyClient` has a `send_to` method that can be used without instantiating the struct.
+
+```rust
+request_group!(MyApi { MyRequest1, MyRequest2 });
+
+let my_client = Client::<MyApi>::new("http://example.com");
+let my_response1 = my_client.send(MyRequest1::new()).await?; // works
+let other_response = my_client.send(OtherRequest::new()).await?; // blocked at compile time
+
+type MyClient = Client::<MyApi>;
+let my_response2 = MyClient::send_to("http://example.com", MyRequest2::new()).await?; // works
+let other_response = MyClient::send_to("http://example.com", OtherRequest::new()).await?; // blocked at compile time
+```
+
+## Other use cases
+If your use case does not meet some of the conditions 2-7 described in the introduction, you'll find my other crate useful, which individually generalizes each of those, allowing any of them to be individually customized with minimal boilerplate. It is currently a work in progress, but almost complete. This crate and that crate will be source-code-compatible, meaning the other crate can be used as a drop-in replacement of this one without changing any code, just with more customization available.
---
+++ src/lib.rs
@@ -0,0 +1,168 @@
+use std::marker::PhantomData;
+
+pub trait Request {
+    /// Type to deserialize from the http response body
+    type Response;
+    /// HTTP method that the request will be sent with
+    fn method(&self) -> HttpMethod;
+    /// String to appended to the end of url when sending this request.
+    fn path(&self) -> String;
+}
+
+/// A client to delegate to the send function that provides the ability to
+/// optionally specify:
+/// - a base url to be used for all requests
+/// - a request group to constrain the request types accepted by this type
+#[derive(Debug, Clone)]
+pub struct Client<RequestGroup = All> {
+    base_url: String,
+    _p: PhantomData<RequestGroup>,
+}
+
+impl<RequestGroup> Client<RequestGroup> {
+    pub fn new(base_url: String) -> Self {
+        Self {
+            base_url,
+            _p: PhantomData,
+        }
+    }
+
+    /// Send the provided request to the host at the specified base url, using
+    /// the request metadata specified by the Request implementation. This
+    /// method upgrades from the `send` function by enabling you to constrain
+    /// the request group with the type system.
+    pub async fn send_to<Req>(base_url: &str, request: Req) -> Result<Req::Response, Error>
+    where
+        Req: Request + serde::Serialize + InRequestGroup<RequestGroup>,
+        Req::Response: for<'a> serde::Deserialize<'a>,
+    {
+        send(base_url, request).await
+    }
+
+    /// Send the provided request to the host at the specified base url, using
+    /// the request metadata specified by the Request implementation. This
+    /// method upgrades from the `send_to` method by allowing you specify the
+    /// base url at the time of instantiation rather than passing it to every
+    /// `send` call.
+    pub async fn send<Req>(&self, request: Req) -> Result<Req::Response, Error>
+    where
+        Req: Request + serde::Serialize + InRequestGroup<RequestGroup>,
+        Req::Response: for<'a> serde::Deserialize<'a>,
+    {
+        send(&self.base_url, request).await
+    }
+}
+
+/// Send the provided request to the host at the specified base url, using the
+/// request metadata specified by the Request implementation.
+pub async fn send<Req>(base_url: &str, request: Req) -> Result<Req::Response, Error>
+where
+    Req: Request + serde::Serialize,
+    Req::Response: for<'a> serde::Deserialize<'a>,
+{
+    let url = join_url(base_url, request.path());
+    send_custom(&url, request.method(), request).await
+}
+
+/// Send the provided request to the host at the specified base url using the
+/// specified method, and deserialize the response as the specified response
+/// type
+pub async fn send_custom<Req, Res>(
+    url: &str,
+    method: HttpMethod,
+    request: Req,
+) -> Result<Res, Error>
+where
+    Req: serde::Serialize,
+    Res: for<'a> serde::Deserialize<'a>,
+{
+    let response = reqwest::Client::new()
+        .request(method.into(), url)
+        .body(serde_json::to_string(&request)?.into_bytes())
+        .send()
+        .await?;
+    let status = response.status();
+    if status.is_success() {
+        let body = response.bytes().await?;
+        Ok(serde_json::from_slice(&body)?)
+    } else {
+        let message = match response.bytes().await {
+            Ok(bytes) => match std::str::from_utf8(&bytes) {
+                Ok(message) => message.to_owned(),
+                Err(e) => format!("failed to parse body: {e:?}"),
+            },
+            Err(e) => format!("failed to get body: {e:?}"),
+        };
+        Err(Error::InvalidStatusCode(status.into(), message))
+    }
+}
+
+/// Define a request group to constrain which requests can be used with a client.
+/// ```ignore
+/// request_group!(MyApi { MyRequest1, MyRequest2 });
+/// ```
+#[macro_export]
+macro_rules! request_group {
+    ($viz:vis $Name:ident { $($Request:ident),*$(,)? }) => {
+        $viz struct $Name;
+        $(impl $crate::request::InRequestGroup<$Name> for $Request {})*
+    };
+}
+
+/// Indicates that a request is part of a request group. If you use the
+/// request_group macro to define the group, it will handle the implementation
+/// of this trait automatically.
+pub trait InRequestGroup<Group> {}
+
+/// The default group. All requests are in this group.
+pub struct All;
+impl<T> InRequestGroup<All> for T {}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("reqwest error: {0}")]
+    ClientError(#[from] reqwest::Error),
+    #[error("serde serialization error: {0}")]
+    SerializationError(#[from] serde_json::error::Error),
+    #[error("invalid status code {0} with response body: `{1}`")]
+    InvalidStatusCode(u16, String),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum HttpMethod {
+    Options,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Head,
+    Trace,
+    Connect,
+    Patch,
+}
+
+impl From<HttpMethod> for reqwest::Method {
+    fn from(value: HttpMethod) -> Self {
+        match value {
+            HttpMethod::Options => reqwest::Method::OPTIONS,
+            HttpMethod::Get => reqwest::Method::GET,
+            HttpMethod::Post => reqwest::Method::POST,
+            HttpMethod::Put => reqwest::Method::PUT,
+            HttpMethod::Delete => reqwest::Method::DELETE,
+            HttpMethod::Head => reqwest::Method::HEAD,
+            HttpMethod::Trace => reqwest::Method::TRACE,
+            HttpMethod::Connect => reqwest::Method::CONNECT,
+            HttpMethod::Patch => reqwest::Method::PATCH,
+        }
+    }
+}
+
+fn join_url(base_url: &str, path: String) -> String {
+    if base_url.chars().last().map(|c| c == '/').unwrap_or(true)
+        || path.chars().next().map(|c| c == '/').unwrap_or(true)
+    {
+        format!("{base_url}{}", path)
+    } else {
+        format!("{base_url}/{}", path)
+    }
+}
